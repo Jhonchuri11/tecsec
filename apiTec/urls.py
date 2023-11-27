b5aefff548e885698df122bb2efdafd692f6f6c4
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2023-11-27 18:50:38 -0500

    Modifando urls.py app, register_user

--- apiTec/urls.py
+++ apiTec/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import RegisterView, LoginView, UserView, LogoutView
+from rest_framework import routers
+
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register_user'),
+    path('register_user/', RegisterView.as_view(), name='register_user'),
     path('login/', LoginView.as_view(), name='login_user'),
     path('user/', UserView.as_view(), name='user'),
     path('logout/', LogoutView.as_view(), name='logout_user')
