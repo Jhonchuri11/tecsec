911200ea4388817df145dc7a55a5d30f7bf2cdf8
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2023-12-08 00:35:33 -0500

    Subiendo el modelo completo de la aplicación

--- apiTec/models.py
+++ apiTec/models.py
@@ -5,13 +5,36 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     numero = models.CharField(max_length=9)
-    username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255, unique=True)
 
     REQUIRED_FIELDS = ['email']
+
+class Calle(models.Model):
+    nombre = models.CharField(max_length=255)
+    latitud = models.DecimalField(max_digits=10, decimal_places=8)
+    longitud = models.DecimalField(max_digits=11, decimal_places=8)
+    nivel_seguridad = models.IntegerField()
+
+
+class Comentario(models.Model):
+    id_calle = models.ForeignKey(Calle, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+class Incidente(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    latitud = models.DecimalField(max_digits=10, decimal_places=8)
+    longitud = models.DecimalField(max_digits=11, decimal_places=8)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    aprobado = models.BooleanField(default=False)
+
+class CallePeligrosa(models.Model):
+    id_calle = models.ForeignKey(Calle, on_delete=models.CASCADE)
+    nivel_peligro = models.IntegerField()
+
 
 class Cliente(models.Model):
     name = models.CharField(max_length=200)
